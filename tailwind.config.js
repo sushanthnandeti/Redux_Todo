ec15cb94c96e483c45a00924808f8753d6aab39a
Sushanth Nandeti <sushanthnandeti96@gmail.com>
2024-04-08 01:53:36 -0400

    Tailwind CSS setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,tsx}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
