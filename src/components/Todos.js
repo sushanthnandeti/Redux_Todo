84a064c2ddcd81e8e5b02a01b10c94ab3f299b52
Sushanth Nandeti <sushanthnandeti96@gmail.com>
2024-05-03 03:44:44 -0400

    Dispatch successful

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { useSelector } from "react-redux";
-
+import { useDispatch} from "react-redux";
+import {removeTodo} from "../features/todo/todoSlice";
+ 
 function todos() {
     const todos = useSelector(state => state.todos)
+    const dispatch = useDispatch();
     console.log(todos);
     return(
         <>
         <div> Todos</div>
         {todos.map((todo) => (
-            
-            <div key = {todo.id}> {todo.text} </div>
+            <ul>
+            <li key = {todo.id}> <div> {todo.text} </div> 
+            <button onClick={() => dispatch(removeTodo(todo.id))}> Remove </button> </li> 
+            </ul>
         ))}
+          
         </>
     )}
 
